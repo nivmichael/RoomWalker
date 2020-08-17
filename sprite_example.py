7da2415c6aefd4df8bec05221093aa531657d1de
AcadeMeData <michael@acade-me.com>
2020-08-17 09:35:35 +0300

    Sprite Added image and some movement

--- sprite_example.py
+++ sprite_example.py
@@ -1,6 +1,7 @@
 # Pygame skeleton for a new game
 import pygame
 import random
+import os
 
 # Window Settings
 WIDTH = 800
@@ -14,20 +15,36 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assets
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
 class Player(pygame.sprite.Sprite):
     # sprite for the player
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
+        # self.image = pygame.Surface((50, 50))
+        # self.image.fill(GREEN)
+        # load player image
+        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
+        self.image.set_colorkey(BLACK)
+        # Create a rect in the size of image - rect is accually the interacting object on the game
         self.rect = self.image.get_rect()
         self.rect.center = (round(WIDTH / 2), round(HEIGHT /2))
+        self.y_speed = 5
 
     def update(self):
         self.rect.x += 5
+        # Important, when adding to Y axis, things go down on screen in pygame
+        self.rect.y += self.y_speed
         # stop from going out of screen
         if self.rect.left > WIDTH:
             self.rect.right = 0
+        # Set the player between y axis of 200-400, switching y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        elif self.rect.top < 200:
+            self.y_speed = 5
 
 # Init game, sound, screen, clock & caption
 pygame.init()
@@ -53,7 +70,7 @@ while running:
     # update
     all_sprites.update()
     # draw / render
-    screen.fill(BLACK)
+    screen.fill(BLUE)
     all_sprites.draw(screen)
     # After drawing everything, flip the display to show it on screen
     pygame.display.flip()
