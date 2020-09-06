b0ceee4fe6fce8ee1480fe2c26892acb04733740
AcadeMeData <michael@acade-me.com>
2020-09-06 09:27:05 +0300

    Added better collisions - circle based

--- shmup.py
+++ shmup.py
@@ -29,6 +29,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (50, 38))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.radius = 20
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -56,9 +58,11 @@ class Mob(pygame.sprite.Sprite):
         self.image = meteor_img
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, YELLOW, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 8)
+        self.speedy = random.randrange(1, 6)
         self.speedx = random.randrange(-3, 3)
 
     def update(self):
@@ -125,7 +129,7 @@ while running:
         all_sprites.add(m)
         mobs.add(m)
     # check if mob collide hit the player
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
     if hits:
         running = False
 
